2f7da5c2da788d4f806249555f3b47209dff6afb
BuhleDamane <ziziphodamane@gmail.com>
2024-05-04 23:16:33 +0200

    Updated all my files

--- script.js
+++ script.js
@@ -1,38 +1,51 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Function to handle button click
+ 
     function buttonClick(event) {
-        event.preventDefault(); // Prevent the default behavior of the link
+        event.preventDefault(); 
         alert("This page is currently under Construction, we apologize for the inconvenience.");
     }
 
-    // Get all elements with class name "law-btn"
     let constructionButtons = document.getElementsByClassName("law-btn");
 
-    // Loop through each button and add click event listener
     for (let i = 0; i < constructionButtons.length; i++) {
         constructionButtons[i].addEventListener("click", buttonClick);
     }
 });
 
-let i = 0;
-let images = [];
-let time = 3000;
 
-images[0] = `display1.jpg`
-images[1] = `display6.jpg`
+let images = ['display1.jpg', 'display6.jpg']; 
+let techImages = ['iced tea.jpg', 'Weather App Screenshot.jpeg', 'Calculator Screenshot (2).jpeg', 'World Clock Screenshot.jpeg', 'Dictionary.jpeg', 'Job Application.jpeg']; // Images for the tech image
+let time1 = 3000; 
+let time2 = 2000; 
+let i1 = 0; 
+let i2 = 0; 
 
-function changeImg(){
-document.display.src = images[i]
+function changeImg() {
+    document.getElementById('display').src = images[i1];
 
-if (i < images.length - 1){
-i++;
+    if (i1 < images.length - 1) {
+        i1++;
+    } else {
+        i1 = 0;
+    }
+    setTimeout(changeImg, time1);
 }
-else{
-    i = 0
-}
-setTimeout("changeImg()", time);
+
+function techImg() {
+    document.getElementById('technology').src = techImages[i2];
+
+    if (i2 < techImages.length - 1) {
+        i2++;
+    } else {
+        i2 = 0;
+    }
+    setTimeout(techImg, time2);
 }
 
-window.onload = changeImg;
+function init() {
+    changeImg();
+    techImg();
+}
 
+window.onload = init;
 
